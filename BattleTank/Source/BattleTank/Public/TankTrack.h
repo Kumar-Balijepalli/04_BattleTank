bfef938f4bbb9181b477b4d70aa744d784b934be
Roro <bsrskumar.28@gmail.com>
2018-11-17 01:43:08 +0530

    Added new member variable which is TrackMaxDrivingForce. (Maximum force required to move the track and subsequently the tank.) Calculating the ForceApplied from the throttle,TrackMaxDrivingForce and the forward vector of the track. Adding the Force calculated above at the location of the tracks.

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -14,6 +14,11 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 public:
+	// Sets a throttle between -1 and +1.
 	UFUNCTION(BlueprintCallable, category = Input)
 	void SetThrottle(float Throttle);
+
+	// Max force per track, in Newtons.
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000.0f; // Assume 40 tonne tank and 1g acceleration (10 m/s^2).
 };
