8be62fd7af721d1da6fb8866876f6fb5c58a9450
Roro <bsrskumar.28@gmail.com>
2018-11-17 01:21:26 +0530

    Added "setThrottle" function. This is BlueprintCallable function under category input. Logging out throttle values based on input.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,3 +2,8 @@
 
 #include "TankTrack.h"
 
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle:%f"),*Name,Throttle);
+}
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -13,5 +13,7 @@ UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintCallable, category = Input)
+	void SetThrottle(float Throttle);
 };
